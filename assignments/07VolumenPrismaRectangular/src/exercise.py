03ab8ba429bc70a08768d0711cbe31e5fff2608d
Blas Treviño <a01177729@itesm.mx>
2021-08-27 10:01:01 -0500

    Corregí el error de Volumen, no se me había gua

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -4,11 +4,11 @@ def volumen_prisma(x, y, z):
 
 def main():
     #escribe tu código abajo de esta línea
-    b = float(input("Dame la base: "))
+    c = float(input("Dame la base: "))
     a = float(input("Dame la altura: "))
     p = float(input("Dame la profundidad: "))
 
-    r = volumen_prisma(b,a,p)
+    r = volumen_prisma(c,a,p)
 
     print("El volumen del prisma es:",r)
 
